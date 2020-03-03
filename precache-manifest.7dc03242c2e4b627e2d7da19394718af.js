a7b2444ef80f3d128566bf3c98b2102360699b3b
luisabad84 <luisabad84@gmail.com>
2020-03-03 10:19:00 -0500

    Updates

---
+++ precache-manifest.7dc03242c2e4b627e2d7da19394718af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2c629397e12121735b76470a9a7a7a",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "efde7d37db8722c4c5d1",
+    "url": "/calculator-app/static/css/main.4419716b.chunk.css"
+  },
+  {
+    "revision": "e6f895877c741ad99bc1",
+    "url": "/calculator-app/static/js/2.6ad45b19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator-app/static/js/2.6ad45b19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "efde7d37db8722c4c5d1",
+    "url": "/calculator-app/static/js/main.59cc37a2.chunk.js"
+  },
+  {
+    "revision": "5de3f27906a5e021eb5f",
+    "url": "/calculator-app/static/js/runtime-main.c9dccca9.js"
+  }
+]);
